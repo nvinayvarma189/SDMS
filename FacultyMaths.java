8a87c0afdffb0f1e386323bc0aa882fbee970569
Vinay Varma <vinay.n.varma189@gmail.com>
2018-03-12 00:31:53 +0530

    Update FacultyMaths.java

--- FacultyMaths.java
+++ FacultyMaths.java
@@ -9,7 +9,7 @@ class FacultyMaths extends StudentDairy
     HashMap<Integer,Integer> studentsmarks1 = new HashMap<Integer,Integer>();
     HashMap<Integer,Integer> studentsmarks2 = new HashMap<Integer,Integer>();
     HashMap<Integer,Integer> studentsmarks3 = new HashMap<Integer,Integer>();
-    public void inputmathsmarks( int number)
+    public void inputmathsmarks( int number) //Maths faculty inputs marks of a student.
     {
       System.out.println("Enter maths periocdical 1 marks");
       mathsmark = s.nextInt();
@@ -21,19 +21,19 @@ class FacultyMaths extends StudentDairy
       mathsmark = s.nextInt();
       studentsmarks3.put(number,mathsmark);
     }
-    public void inputmathsattendance(int number)
+    public void inputmathsattendance(int number) //Maths faculty inputs attendance of a student.
     {
       System.out.println("Enter maths Attendance");
       mathsattendance = s.nextInt();
       studentsattendance.put(number,mathsattendance);
     }
-		public void getmathsmarks(int number)
+    public void getmathsmarks(int number) //To view the academic details of a student.
     {
       System.out.println("Periodical 1 marks maths: "+ studentsmarks1.get(number));
       System.out.println("Periodical 2 marks maths: "+ studentsmarks2.get(number));
       System.out.println("End semester marks maths: "+ studentsmarks3.get(number));
 			try{
-				if(studentsattendance.get(number) < 75)
+				if(studentsattendance.get(number) < 75) //checking attendance shortage.
 				{
 					System.out.println("Your attendance is "+studentsattendance.get(number)+" and you are not qualified to write the exam");
 				}
