7d46c36a48f5eec617d78f071f72964d3592eafd
Vinay Varma <vinay.n.varma189@gmail.com>
2018-03-12 00:28:12 +0530

    Update FacultyDsa.java

--- FacultyDsa.java
+++ FacultyDsa.java
@@ -9,7 +9,7 @@ class FacultyDsa extends StudentDairy
     HashMap<Integer,Integer> studentsmarks1 = new HashMap<Integer,Integer>();
     HashMap<Integer,Integer> studentsmarks2 = new HashMap<Integer,Integer>();
     HashMap<Integer,Integer> studentsmarks3 = new HashMap<Integer,Integer>();
-    public void inputdsamarks( int number)
+    public void inputdsamarks( int number) //DS faculty input marks for a student.
     {
       System.out.println("Enter dsa periocdical 1 marks");
       dsamark = s.nextInt();
@@ -21,20 +21,20 @@ class FacultyDsa extends StudentDairy
       dsamark = s.nextInt();
       studentsmarks3.put(number,dsamark);
     }
-    public void inputdsaAttendance(int number)
+    public void inputdsaAttendance(int number) //DS faculty input attendance for a student.
     {
       System.out.println("Enter dsa Attendance");
       dsaattendance = s.nextInt();
       studentsattendance.put(number,dsaattendance);
     }
-    public void getdsamarks(int number)
+    public void getdsamarks(int number) //To view the academic details of a student.
     {
       System.out.println("Periodical 1 marks Dsa: "+ studentsmarks1.get(number));
       System.out.println("Periodical 2 marks Dsa: "+ studentsmarks2.get(number));
       System.out.println("End semester marks Dsa: "+ studentsmarks3.get(number));
 			try
 			{
-				if(studentsattendance.get(number) < 75)
+				if(studentsattendance.get(number) < 75) //checking attendance shortage.
 				{
 					System.out.println("Your attendance is "+studentsattendance.get(number)+" and you are not qualified to write the exam");
 				}
